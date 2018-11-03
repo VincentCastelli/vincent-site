ea289e7e44576f0d82a27e3cc7f27683d04c209d
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-11-03 14:30:55 -0700

    Added contact me component with contact info.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
-      <section className="showcase">
-        <h1>The Contact Page</h1>
-      </section>
+    <div className="contact-main">
+      <h1 className="lg-heading">
+        Contact&nbsp;
+        <span className="text-secondary">
+          Me
+        </span>
+      </h1>
+      <div className="contact-info">
+        <div>
+          <span className="text-secondary">
+            e:&nbsp;
+          </span>
+          vincentsalvatorecastelli@gmail.com
+        </div>
+        <div>
+          <span className="text-secondary">
+            p:&nbsp;
+          </span>
+          718-724-4386
+        </div>
+      </div>
     </div>
   );
 };
