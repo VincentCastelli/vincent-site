bef5a7525a32d4fd412feb2d6172cbd6174d9f1c
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-11-05 11:44:49 -0800

    Modifications to background overlay; added link to resume;  finished contact me component.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,15 +12,23 @@ const Contact = () => {
       <div className="contact-info">
         <div>
           <span className="text-secondary">
-            e:&nbsp;
+            <i className="fas fa-envelope">&nbsp;&nbsp;</i>
           </span>
           vincentsalvatorecastelli@gmail.com
         </div>
         <div>
           <span className="text-secondary">
-            p:&nbsp;
+            <i className="fas fa-phone">&nbsp;&nbsp;</i>
           </span>
           718-724-4386
+        </div>
+        <div>
+          <a href="https://docs.google.com/document/d/1E4it7w8Bz7jZMUWMNiB0cHNRluC0i-8GVlrvD_OppiA/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+            <span className="text-secondary">
+              <i className="fas fa-file">&nbsp;&nbsp;</i>
+            </span>
+            View Resume
+          </a>
         </div>
       </div>
     </div>
